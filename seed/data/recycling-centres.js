3ae0e55e65690ecc98b825f0a551e76af7e58ce1
William Pickering <willpicky@gmail.com>
2017-05-02 13:32:07 +0100

    More recycling centres added to database

--- seed/data/recycling-centres.js
+++ seed/data/recycling-centres.js
@@ -68,5 +68,75 @@ module.exports = [
     postcode: 'M26 4FT',
     latitude: 53.5679335,
     longitude: -2.3345166999999947
+  },
+  {
+    name: 'Salford Road Recycling Centre',
+    address: 'Salford Road Recycling Centre, Salford Road, Bolton, Bl5 1DG',
+    postcode: 'Bl5 1DG',
+    latitude: 53.5377641,
+    longitude: -2.4459646999999904
+  },
+  {
+    name: 'Hurstwood Court Recycling Centre',
+    address: 'Hurstwood Court Recycling Centre, Raikes Lane Industrial Estate, Bolton, BL3 2NH',
+    postcode: 'BL3 2NH',
+    latitude: 53.5655455,
+    longitude: -2.4087372000000187
+  },
+  {
+    name: 'Every Street Recycling Centre',
+    address: 'Every Street Recycling Centre, Every Street, Bury, BL9 5BE',
+    postcode: 'BL9 5BE',
+    latitude: 53.60180519999999,
+    longitude: -2.295570399999974
+  },
+  {
+    name: 'Arkwright Street Recycling Centre',
+    address: 'Arkwright Street Recycling Centre, Arkwright Street, Oldham, OL9 9LZ',
+    postcode: 'OL9 9LZ',
+    latitude: 53.54146249999999,
+    longitude: -2.1321034000000054
+  },
+  {
+    name: 'Chichester Street Recycling Centre',
+    address: 'Chichester Street Recycling Centre, Chichester Street, Rochdale OL16 2AU',
+    postcode: 'OL16 2AU',
+    latitude: 53.6136493,
+    longitude: -2.1460087000000385
+  },
+  {
+    name: 'Adswood Road Recycling Centre',
+    address: 'Adswood Road Recycling Centre, Adswood Road, Cheadle Hulme, SK8 5QY',
+    postcode: 'SK8 5QY',
+    latitude: 53.3872479,
+    longitude: -2.1737984999999753
+  },
+  {
+    name: 'Bredbury Parkway Recycling Centre',
+    address: 'Bredbury Parkway Recycling Centre, Bredbury Park Way, Bredbury Park Industrial Estate, Bredbury, SK6 2SN',
+    postcode: 'SK6 2SN',
+    latitude: 53.4300232,
+    longitude: -2.115547699999979
+  },
+  {
+    name: 'Railway Road Recycling Centre',
+    address: 'Railway Road Recycling Centre, Railway Road, Marple, SK6 6HU',
+    postcode: 'SK6 6HU',
+    latitude: 53.3952776,
+    longitude: -2.077076199999965
+  },
+  {
+    name: 'Bayley Street Recycling Centre',
+    address: 'Bayley Street Recycling Centre, Bayley Street, Stalybridge, SK15 1PZ',
+    postcode: 'SK15 1PZ',
+    latitude: 53.4828398,
+    longitude: -2.068495299999995
+  },
+  {
+    name: 'Sinderland Road Recycling Centre',
+    address: 'Sinderland Road Recycling Centre, Woodhouse Lane (off Sinderland Road), Altrincham, WA14 5TB',
+    postcode: 'WA14 5TB',
+    latitude: 53.4112842,
+    longitude: -2.3733471000000463
   }
 ];
